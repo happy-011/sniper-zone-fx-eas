4d723b08fc495878088d0958776e60f249da3855
happy-011 <khanthappy53@gmail.com>
2026-07-22 16:02:17 +0530

    data.js

--- js/data.js
+++ js/data.js
@@ -1,27 +1,47 @@
-const expertAdvisors = [
+const expertAdvisors=[
 
 {
-    id:1,
-    name:"SNIPER PRO EA",
-    price:"$99",
-    image:"images/ea1/cover.webp",
-    page:"ea/ea1.html"
+
+id:1,
+
+name:"SNIPER PRO EA",
+
+price:"$99",
+
+description:"Professional MT5 Expert Advisor.",
+
+cover:"../images/ea1/cover.webp",
+
+report1:"../images/ea1/report1.webp",
+
+report2:"../images/ea1/report2.webp",
+
+report3:"../images/ea1/report3.webp",
+
+report4:"../images/ea1/report4.webp"
+
 },
 
 {
-    id:2,
-    name:"GOLD SCALPER EA",
-    price:"$149",
-    image:"images/ea2/cover.webp",
-    page:"ea/ea2.html"
-},
 
-{
-    id:3,
-    name:"XAU MASTER EA",
-    price:"$129",
-    image:"images/ea3/cover.webp",
-    page:"ea/ea3.html"
+id:2,
+
+name:"GOLD SCALPER EA",
+
+price:"$149",
+
+description:"Gold Scalping Expert Advisor.",
+
+cover:"../images/ea2/cover.webp",
+
+report1:"../images/ea2/report1.webp",
+
+report2:"../images/ea2/report2.webp",
+
+report3:"../images/ea2/report3.webp",
+
+report4:"../images/ea2/report4.webp"
+
 }
 
 ];
