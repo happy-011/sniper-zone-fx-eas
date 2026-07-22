5243b29bcf4c258948cbf6775414e6eda1cf66ca
happy-011 <khanthappy53@gmail.com>
2026-07-22 15:55:31 +0530

    data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,27 @@
+const expertAdvisors = [
 
+{
+    id:1,
+    name:"SNIPER PRO EA",
+    price:"$99",
+    image:"images/ea1/cover.webp",
+    page:"ea/ea1.html"
+},
+
+{
+    id:2,
+    name:"GOLD SCALPER EA",
+    price:"$149",
+    image:"images/ea2/cover.webp",
+    page:"ea/ea2.html"
+},
+
+{
+    id:3,
+    name:"XAU MASTER EA",
+    price:"$129",
+    image:"images/ea3/cover.webp",
+    page:"ea/ea3.html"
+}
+
+];
